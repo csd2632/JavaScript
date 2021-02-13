76cd1821e3985e3a95f64f3e85f8e30f8065d853
nikolaou_ap <nikolaou_ap@ymail.com>
2021-02-13 21:55:30 +0200

    add images dynamically to html

--- Item.js
+++ Item.js
@@ -6,12 +6,15 @@ class Item {
         el_descr.innerHTML='Description: '+ charact[1];
         let el_year = document.createElement('div');
         el_year.innerHTML='Year: '+ charact[2];
+        let el_img = document.createElement('img');
+        el_img.src = './img/'+ charact[0]+'.bmp';
+
    
    
         this.element=document.createElement('div');  
         this.element.appendChild(el_name);
         this.element.appendChild(el_descr);
         this.element.appendChild(el_year);
-        
+        this.element.appendChild(el_img);
     }
 }
--- main.js
+++ main.js
@@ -3,4 +3,16 @@ function addImage2Body(charact) {
     document.body.appendChild(item.element);
 
 }
-addImage2Body(['name','decription','year']);
+function addImages(){
+    let i;
+    let data = [
+        {name:'0380_00800S',descr:"one",year:'2001'},
+        {name:'0390_01000S',descr:"two",year:'2003'},
+        {name:'0400_01726S',descr:"three",year:'2002'}
+    ];
+    for(i=0;i<data.length;i++){
+        addImage2Body([data[i].name,data[i].descr,data[i].year]);
+
+    }
+}
+addImages();
